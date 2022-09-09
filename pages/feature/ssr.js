94ec3895ca72fbbf5a00280ecaf0b13f7862ecde
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2022-09-09 12:58:05 +0530

    Update ssr.js

--- pages/feature/ssr.js
+++ pages/feature/ssr.js
@@ -1,16 +1,21 @@
+import Clock from "../components/Clock";
+
 export default function SSRDemo(props) {
-   return (
+	return (
 	   <>
+	   <div>
+			<Clock></Clock>
+		</div>
 		  <main>
-			<h1>Server Side Rendering</h1>
-			<h2>
+			<h1 style={{ fontSize: "55px", margin: "20px", textAlign: "center" }}>Server Side Rendering</h1>
+			<h2 style={{ fontSize: "25px", margin: "20px", textAlign: "center" }}>
 			  <div>Next Date-Time: {props.dateTime}</div>
 			</h2>
-			<h2>
-            <a href="/">
-               Home
-            </a>
-         </h2>
+			<h2 style={{ fontSize: "25px", margin: "10%", textAlign: "center" }}>
+				<a href="/">
+					Home
+				</a>
+			</h2>
 		  </main>
 	   </>
 	);
