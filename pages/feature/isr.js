3ced97a778356fceb073c69939711e6f31e321a2
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2022-09-09 12:57:47 +0530

    Update isr.js

--- pages/feature/isr.js
+++ pages/feature/isr.js
@@ -1,16 +1,27 @@
+import Clock from "../components/Clock";
+
 export default function ISRDemo(props) {
    return (
 	   <>
+	   <div>
+			<Clock></Clock>
+		</div>
 		  <main>
-			<h1>Incremental Static Regeneration</h1>
-			<h2>
+			<h1 style={{ fontSize: "55px", margin: "20px", textAlign: "center" }}>Incremental Static Regeneration</h1>
+			<h2 style={{ fontSize: "25px", margin: "20px", textAlign: "center" }}>
+			  (Reload after 15 seconds)
+			</h2>
+			<h2 style={{ fontSize: "25px", margin: "20px", textAlign: "center" }}>
+			  (revalidate interval: 15 seconds)
+			</h2>
+			<h2 style={{ fontSize: "25px", margin: "20px", textAlign: "center" }}>
 			  <div>Next Date-Time: {props.dateTime}</div>
 			</h2>
-			<h2>
-            <a href="/">
-               Home
-            </a>
-         </h2>
+			<h2 style={{ fontSize: "25px", margin: "10%", textAlign: "center" }}>
+				<a href="/">
+					Home
+				</a>
+			</h2>
 		  </main>
 	   </>
 	);
