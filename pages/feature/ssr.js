7a59a64abc4516ec8396d40db77b2a017ef7b3ed
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2022-10-07 01:02:54 +0530

    Update ssr.js

--- pages/feature/ssr.js
+++ pages/feature/ssr.js
@@ -1,17 +1,15 @@
-import Clock from "../components/Clock";
-
 export default function SSRDemo(props) {
 	return (
 	   <>
 	   <div>
-			<Clock></Clock>
+			
 		</div>
 		  <main>
-			<h1 style={{ fontSize: "55px", margin: "20px", textAlign: "center" }}>Server Side Rendering</h1>
-			<h2 style={{ fontSize: "25px", margin: "20px", textAlign: "center" }}>
-			  <div>Next Date-Time: {props.dateTime}</div>
+			<h1 style={{ fontSize: "60px", margin: "20px", textAlign: "center" }}>Server Side Rendering</h1>
+			<h2 style={{ fontSize: "70px", margin: "5%", textAlign: "center" }}>
+			  <div>{props.thought}</div>
 			</h2>
-			<h2 style={{ fontSize: "25px", margin: "10%", textAlign: "center" }}>
+			<h2 style={{ fontSize: "40px", margin: "10%", textAlign: "center" }}>
 				<a href="/">
 					Home
 				</a>
@@ -22,10 +20,15 @@ export default function SSRDemo(props) {
 }
 
 export async function getServerSideProps() {
-
-   return {
-    props: { 
-	    dateTime: new Date().toISOString() 
-    },
-  }
+	const ThoughtList = ['"You have to dream before your dreams can come true." – A.P.J Abdul Kalam', 
+						'"Life should be great rather than long." – B.R. Ambedkar',
+						'“Education is the most powerful weapon which you can use to change the world.” - Nelson Mandela',
+						'“Every champion was once a contender that didn’t give up.” ― Gabby Douglas',
+						'“Nothing will work unless you do.” ― Maya Angelou'];
+	let index = Math.ceil(Math.random() * 10) % 5;
+    return {   
+		props: {
+			  thought: ThoughtList[index],
+			},
+    }
 }
