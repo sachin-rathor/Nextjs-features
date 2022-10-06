3381c75e56a5ef7aa0bc606d8b86e945d84c28c7
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2022-10-07 01:01:59 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import Clock from "./components/Clock";
 
 function HomePage() {
 	useEffect(() => {
@@ -8,12 +7,12 @@ function HomePage() {
    return (
       <>
 		<div>
-			<Clock></Clock>
+			
 		</div>
-         <div style={{ fontSize: "55px", margin: "20px", textAlign: "center" }}>Welcome to Next.js!</div>
-		 <div style={{ padding: "10px 20px", fontSize: "25px", margin: "5%", textAlign: "center", color: "white"}}>
+         <div style={{ fontSize: "120px", margin: "20px", textAlign: "center" }}>Welcome to Next.js!</div>
+		 <div style={{ padding: "10px 20px", fontSize: "50px", margin: "5%", textAlign: "center", color: "white"}}>
 			<a href="/feature/ssr">SSR Demo</a><br></br><br></br>
-			<a href="/feature/isr">ISR Demo with revalidate time of 15s.</a>
+			<a href="/feature/isr">ISR Demo with revalidate time of 10s.</a>
 		 </div>
       </>	    
    )
