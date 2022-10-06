11d220b470e12f0421815a2ab09e59e10c858407
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2022-10-07 01:03:21 +0530

    Update isr.js

--- pages/feature/isr.js
+++ pages/feature/isr.js
@@ -1,23 +1,20 @@
-import Clock from "../components/Clock";
-
 export default function ISRDemo(props) {
    return (
 	   <>
 	   <div>
-			<Clock></Clock>
 		</div>
 		  <main>
-			<h1 style={{ fontSize: "55px", margin: "20px", textAlign: "center" }}>Incremental Static Regeneration</h1>
+			<h1 style={{ fontSize: "60px", margin: "20px", textAlign: "center" }}>Incremental Static Regeneration</h1>
 			<h2 style={{ fontSize: "25px", margin: "20px", textAlign: "center" }}>
-			  (Reload after 15 seconds)
+			  (Reload after 10 seconds)
 			</h2>
 			<h2 style={{ fontSize: "25px", margin: "20px", textAlign: "center" }}>
-			  (revalidate interval: 15 seconds)
+			  (revalidate interval: 10 seconds)
 			</h2>
-			<h2 style={{ fontSize: "25px", margin: "20px", textAlign: "center" }}>
-			  <div>Next Date-Time: {props.dateTime}</div>
+			<h2 style={{ fontSize: "70px", margin: "20px", textAlign: "center" }}>
+			  <div>{props.thought}</div>
 			</h2>
-			<h2 style={{ fontSize: "25px", margin: "10%", textAlign: "center" }}>
+			<h2 style={{ fontSize: "40px", margin: "10%", textAlign: "center" }}>
 				<a href="/">
 					Home
 				</a>
@@ -28,10 +25,16 @@ export default function ISRDemo(props) {
 }
 
 export async function getStaticProps() {
+	const ThoughtList = ['"You have to dream before your dreams can come true." – A.P.J Abdul Kalam', 
+						'"Life should be great rather than long." – B.R. Ambedkar',
+						'“Education is the most powerful weapon which you can use to change the world.” - Nelson Mandela',
+						'“Every champion was once a contender that didn’t give up.” ― Gabby Douglas',
+						'“Nothing will work unless you do.” ― Maya Angelou'];
+	let index = Math.ceil(Math.random() * 10) % 5;
 	return {
 		props: {
-		  dateTime: new Date().toISOString(),
+		  thought: ThoughtList[index],
 		},
-		revalidate: 15, // In seconds
+		revalidate: 10, // In seconds
 	}
 }
