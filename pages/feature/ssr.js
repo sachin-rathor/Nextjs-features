30ea70c84370eda0ae6573e2dad39e5f97251c7c
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2022-10-07 21:44:51 +0530

    Update ssr.js

--- pages/feature/ssr.js
+++ pages/feature/ssr.js
@@ -24,8 +24,13 @@ export async function getServerSideProps() {
 						'"Life should be great rather than long." – B.R. Ambedkar',
 						'“Education is the most powerful weapon which you can use to change the world.” - Nelson Mandela',
 						'“Every champion was once a contender that didn’t give up.” ― Gabby Douglas',
+			     			'“A little progress each day adds up to big results.” – Satya Nani',
+			     			'“Every accomplishment starts with the decision to try.” – Gail Devers',
+			     			'“If you can dream it, you can do it.” - Walt Disney',
+			     			'“All of us do not have equal talent. But, all of us have an equal opportunity to develop our talents.” ― A.P.J. Abdul Kalam',
+			     			'“A person who never made a mistake never tried anything new.” — Albert Einstein',
 						'“Nothing will work unless you do.” ― Maya Angelou'];
-	let index = Math.ceil(Math.random() * 10) % 5;
+	let index = Math.ceil(Math.random() * 10) % 10;
     return {   
 		props: {
 			  thought: ThoughtList[index],
